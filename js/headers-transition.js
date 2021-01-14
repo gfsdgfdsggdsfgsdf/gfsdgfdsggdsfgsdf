90b7707e51dcb7561731a508820db68edd5362e4
gfsdgfdsggdsfgsdf <77353338+gfsdgfdsggdsfgsdf@users.noreply.github.com>
2021-01-14 13:07:59 +0300

    Add files via upload

--- js/headers-transition.js
+++ js/headers-transition.js
@@ -1,12 +1,12 @@
 /* Создаёт красивое появление текста на логотипе */
 
-let firstHeader = document.querySelector(".logo h2");
-let secondHeader = document.querySelector(".logo h1");
+let firstHeader = document.querySelector(".logo h1");
+// let secondHeader = document.querySelector(".logo h2");
 
 document.addEventListener("DOMContentLoaded", function(e) {
 	firstHeader.style.transform = "translateX(0px)";
 	firstHeader.style.opacity = 1;
 
-	secondHeader.style.transform = "translateX(0px)";
-	secondHeader.style.opacity = 1;
+	// secondHeader.style.transform = "translateX(0px)";
+	// secondHeader.style.opacity = 1;
 });
